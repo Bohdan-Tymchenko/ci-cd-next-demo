12a6f7d9e6f9e82742ecbc96951541bae0d4bb72
BohdanTymchenko <tymchenkobwork@gmail.com>
2026-06-13 10:28:26 +0100

    Enhance homepage layout by adding CV download link and adjusting styles for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold sm:text-3xl">
           Bohdan Tymchenko 🚀
         </h1>
-        <div className="w-fit mt-4">
+        <div className="w-fit mt-4 gap-5 flex sm:flex-row flex-col">
           <a
             className="inline-flex items-center gap-2 rounded-md border px-3 py-1.5 text-sm font-medium hover:bg-foreground hover:text-background"
             href="https://www.linkedin.com/in/bohdan-tymchenko-0862a381/"
@@ -18,15 +18,18 @@ export default function Home() {
           >
             <ArrowRightIcon className="w-4 h-4" /> Go to my LinkedIn profile
           </a>
+          <a className="inline-flex items-center gap-2 rounded-md border px-3 py-1.5 text-sm font-medium hover:bg-foreground hover:text-background" href={CV_FILE} download>
+            <ArrowRightIcon className="w-4 h-4" /> Download my CV
+          </a>
         </div>
 
       </header>
 
-      <section className="flex flex-col gap-3">
+      <section className="flex flex-col gap-3 justify-start">
         <PdfViewer
           src={CV_FILE}
           title="Bohdan Tymchenko CV"
-          className="h-[80vh] w-full rounded-lg border"
+          className="h-[80vh] w-full rounded-lg border mt-4"
         />
       </section>
     </main>
